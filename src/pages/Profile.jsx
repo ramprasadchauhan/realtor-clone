93af74451e9dc6cf9e629924361955f765f93b2d
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-22 23:49:18 +0530

    add edit functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 const Profile = () => {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -14,6 +18,30 @@ const Profile = () => {
     auth.signOut();
     navigate("/");
   };
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update the displayname in firebade auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        //  upade name in firestore
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile details updated");
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
+  };
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -22,11 +50,14 @@ const Profile = () => {
           <form>
             {/* Name input */}
             <input
+              onChange={onChange}
               type="text"
               id="name"
               value={name}
-              disabled
-              className="w-full rounded px-4 mb-6 py-2 text-xl text-gray-700 bg-white border border-gray-300 transition ease-in-out"
+              disabled={!changeDetail}
+              className={`w-full rounded px-4 mb-6 py-2 text-xl text-gray-700 bg-white border border-gray-300 transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              } `}
             />
             {/* Email input */}
             <input
@@ -40,8 +71,14 @@ const Profile = () => {
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center">
                 Do you want to chage name?
-                <span className="text-red-600 hover:text-red-700 duration-200 transition ml-1 cursor-pointer ease-in-out">
-                  Edit
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 duration-200 transition ml-1 cursor-pointer ease-in-out"
+                >
+                  {changeDetail ? "Apply chage" : "Edit"}
                 </span>
               </p>
               <p
