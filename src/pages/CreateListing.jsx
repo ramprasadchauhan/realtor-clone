077419da2cdaab7606c0ccc3640d9c3d51bb8be1
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-02-23 17:15:04 +0530

    Add my listing section to the profile page and create listing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -157,6 +157,7 @@ const CreateListing = () => {
       imgUrls,
       geolocation,
       timestamp: serverTimestamp(),
+      userRef: auth.currentUser.uid,
     };
     delete formDataCopy.images;
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
